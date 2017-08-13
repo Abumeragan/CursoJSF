be4463057d5a41b82f06035ef63ef57948b09ef4
Abumeragan <rodrigo.uba@gmail.com>
2017-08-13 16:07:57 -0300

    Implementação CRUD parte 1

---
+++ src/br/com/framework/implementacao/crud/SimpleJdbcClassImpl.java
@@ -0,0 +1,11 @@
+package br.com.framework.implementacao.crud;
+
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
+@Component
+@Transactional(propagation = Propagation.REQUIRED, rollbackFor = Exception.class)
+public class SimpleJdbcClassImpl {
+
+}
